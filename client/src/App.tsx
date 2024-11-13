8d4c51ec2664375cad03e1984a7788f0b22b89ec
Apoorva Kashyap <apkpashu@gmail.com>
2024-11-13 15:37:34 +0530

    useState, useEffect, cors

---
+++ client/src/App.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+function App() {
+
+  const [products, setProducts] = useState([{ name: 'product1', price: 100.00 },
+  { name: 'product2', price: 200.00 }]);
+   
+  useEffect(()=>{
+     fetch('http://localhost:5000/api/products').then(response=> response.json()).then(data=>setProducts(data))
+  },[]);
+
+
+  function addProducts() {
+    setProducts(prevState => [...prevState, { name: 'product' + (prevState.length + 1), price: (prevState.length * 100) + 100 }])
+  }
+
+  return (
+    <div className='app'>
+      <h1>Re-Store</h1>
+      <ul>
+        {products.map(item => (
+          <li key={item.name}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProducts}>Add product</button>
+    </div>
+  );
+}
+
+export default App;
