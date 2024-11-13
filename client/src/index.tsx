a80a297189307f9074dd293e3c7bd04f40098eae
Apoorva Kashyap <apkpashu@gmail.com>
2024-11-13 16:30:03 +0530

    correct path

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import '../../client/src/app/layout/style.css'
+import App from '../../client/src/app/layout/App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
